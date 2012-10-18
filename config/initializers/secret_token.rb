d84eab282b79ac08c3a0251fe504653f6dae7a83
Ron Rillo <ron.rillo@gmail.com>
2012-10-17 23:45:36 -0700

    Initial commit - Rails is the Best

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'f5425a44b42e5ef63f096ae3e65f364dcd7f7187ef69586d441876c8b97913417a07d9153eca5afde327caadfbb8aafbacc34defccaa92a7a3312ea42de9371a'
